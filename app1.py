ccd58e28aefa800e1251a2f62a159d143869e623
Wwilscada <Sachin.yadav@windworldindia.com>
2025-06-24 16:13:11 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -252,5 +252,5 @@ scheduler.start()
 
 # 🔥 Main entrypoint
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7738))  # Use PORT from environment if set
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
